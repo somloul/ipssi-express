24b14b0c16ee8df579a7c415ee15d5065629e566
Teepan <paartheepan94@gmail.com>
2023-01-24 10:20:35 +0100

    feat: add db migration

---
+++ prisma/migrations/20230124091445_init/migration.sql
@@ -0,0 +1,44 @@
+-- CreateTable
+CREATE TABLE "TodoList" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "TodoList_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TodoItem" (
+    "id" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "todoListId" TEXT NOT NULL,
+
+    CONSTRAINT "TodoItem_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" VARCHAR(64) NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "TodoList_id_key" ON "TodoList"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "TodoItem_id_key" ON "TodoItem"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- AddForeignKey
+ALTER TABLE "TodoList" ADD CONSTRAINT "TodoList_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TodoItem" ADD CONSTRAINT "TodoItem_todoListId_fkey" FOREIGN KEY ("todoListId") REFERENCES "TodoList"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
